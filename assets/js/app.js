43773100153f9570a0d241823215ec709867dd03
Angie Condor <angiegita1602@gmail.com>
2017-04-11 18:45:53 -0500

    segundo avance

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,24 @@
-windows.onload= function(){
-  var modalPopup = document.getElementById("modal-popup");
-  var img = document.getElementsByClassName("img");
+window.addEventListener("load",function(){
+  var modalPopup = document.getElementById("popup-contenedor");
+  // var img = document.getElementsByClassName("img");
   var img1 = document.getElementById("img1");
   var modalImg = document.getElementById("modal-img");
 
-  img1.onclick = function(){
-    // for(var i = 0; i <= img.length; i++){
-    //   img[i]
+  // img1.onclick = function(){
+  //   // for(var i = 0; i <= img.length; i++){
+  //   //   img[i]
+  //   modalPopup.style.display = "block";
+  //   modalImg.setAttribute("src",this.src);
+  // }
+
+  img1.addEventListener("click",function(e){
+    e.preventDefault();
     modalPopup.style.display = "block";
-    modalImg.src = this.src;
-  }
+    modalImg.setAttribute("src",this.src);
+  });
 
   var close = document.getElementsByClassName("close")[0];
   close.onclick = function(){
     modalPopup.style.display = "none";
   }
-}
+});
